4fcf5e500ef8e1a55c7208602c05c00b1e673b8f
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-08-30 15:18:16 +0000

    added additional attributes to `shops` table

--- db/migrate/20210809103903_create_shops.rb
+++ db/migrate/20210809103903_create_shops.rb
@@ -3,6 +3,8 @@ class CreateShops < ActiveRecord::Migration[6.1]
     create_table :shops do |t|
       t.string :name
       t.string :address
+      t.string :email
+      t.string :currency
       t.references :organization
       t.references :created_by, null: false, foreign_key: {to_table: :users}
       t.references :updated_by, null: false, foreign_key: {to_table: :users}
