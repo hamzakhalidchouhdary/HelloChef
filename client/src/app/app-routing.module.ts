f5538622eafa52637457c1194a296dfd7934fbb1
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-04 15:49:23 +0000

    added staff and product routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { StaffComponent } from './staff/staff.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: 'staffs', component: StaffComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
