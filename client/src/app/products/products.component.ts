aaeb5e85293714bd54bb40701ce217373af662af
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-06 18:00:14 +0000

    raise notification when error occure

--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../services/product/product.service';
+import { NotificationService } from '../services/notification/notification.service';
 
 @Component({
   selector: 'app-products',
@@ -7,12 +8,15 @@ import { ProductService } from '../services/product/product.service';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private notificationService: NotificationService) {}
 
   ngOnInit() {
     this.productService.fetchProducts().subscribe(
       (data) => { console.log(data); },
-      (error) => { console.error(error); }
+      (error) => { 
+        console.error(error); 
+        this.notificationService.showSuccess('error occure')
+      }
     );
   }
 }
