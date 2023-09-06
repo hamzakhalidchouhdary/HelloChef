2ee9876d9c54ca49815160d878f4307b5b2b46ab
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-06 17:39:09 +0000

    call product fetch method

--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product/product.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  constructor(private productService: ProductService) {}
 
+  ngOnInit() {
+    this.productService.fetchProducts().subscribe(
+      (data) => { console.log(data); },
+      (error) => { console.error(error); }
+    );
+  }
 }
