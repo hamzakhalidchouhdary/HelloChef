dff92c6e4a1d6b856ee8c84d8411c1b5475dc32e
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-06 17:38:36 +0000

    define product fetch method

--- client/src/app/services/product/product.service.ts
+++ client/src/app/services/product/product.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService {}
+export class ProductService {
+  private productUrl = 'http://127.0.0.1:57056/api/v1/item';
+
+  constructor(private httpClient: HttpClient) { }
+
+  fetchProducts(): Observable<any> {
+    return this.httpClient
+      .get<any>(this.productUrl)
+      .pipe(catchError((error: any) => {
+        return throwError(() => {
+          let err = new Error('error in fetching products');
+          err.message = error.error.error;
+          return err;
+        });
+      })
+    );
+  }
+}
