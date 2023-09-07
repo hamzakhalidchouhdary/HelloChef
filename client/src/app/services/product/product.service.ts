7973aaf208189dd90229bb54be1243cf328ee731
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-07 18:09:11 +0000

    updated items fetch call added auth header.

--- client/src/app/services/product/product.service.ts
+++ client/src/app/services/product/product.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private productUrl = 'http://127.0.0.1:57056/api/v1/item';
+  private productUrl = 'http://127.0.0.1:41000/api/v1/item?shop_id=1';
 
   constructor(private httpClient: HttpClient) { }
 
   fetchProducts(): Observable<any> {
+    const token = `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjpudWxsLCJleHAiOjE2OTQxOTM0MjV9.UNAsezEO6pW8uQz8ugKThs-dlUcnoULayISRcsfsjOg`
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': token
+    })
     return this.httpClient
-      .get<any>(this.productUrl)
+      .get<any>(this.productUrl, {headers: header})
       .pipe(catchError((error: any) => {
         return throwError(() => {
           let err = new Error('error in fetching products');
