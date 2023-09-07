59c8a86e5d7ca3df77ef5616fd5c05e4b3a10af5
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-07 18:07:44 +0000

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create({
+  first_name: 'Hamza',
+  last_name: 'Khalid',
+  username: 'hamza',
+  password_digest: '12345',
+  email: 'hamza@helloChef.com',
+  created_by_id: 1,
+  updated_by_id: 1
+});
+organization = Organization.create({ 
+  name: 'KK Group',
+  created_by_id: user.id,
+  updated_by_id: user.id
+});
+shop = Shop.create({
+  name: 'KK Store',
+  organization_id: organization.id,
+  created_by_id: user.id,
+  updated_by_id: user.id
+});
+Item.create([
+  {shop_id: shop.id, title: 'dummy product 1', created_by_id: user.id, updated_by_id: user.id},
+  {shop_id: shop.id, title: 'dummy product 2', created_by_id: user.id, updated_by_id: user.id},
+  {shop_id: shop.id, title: 'dummy product 3', created_by_id: user.id, updated_by_id: user.id},
+  {shop_id: shop.id, title: 'dummy product 4', created_by_id: user.id, updated_by_id: user.id}
+]);
