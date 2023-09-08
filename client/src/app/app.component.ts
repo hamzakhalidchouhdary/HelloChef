513f45a991bbd5b20fb2a9ed904dafeff02aa52b
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-08 17:09:55 +0000

    added nav item activation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+
+  isRootRouteActive: boolean = false;
+  constructor (private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isRootRouteActive = this.router.url === '/';
+      }
+    });
+  }
 }
