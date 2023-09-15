26aac8327f65f702882f3423b242be14848d1f54
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-09-15 20:03:28 +0000

    added dialog method in `edit-product` component

--- client/src/app/components/edit-product/edit-product.component.ts
+++ client/src/app/components/edit-product/edit-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-product',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-product.component.scss']
 })
 export class EditProductComponent {
+  constructor (private dialogRef: MatDialogRef<EditProductComponent>) {}
 
+  save() :void {
+    console.log('product saved');
+    this.close();
+  }
+
+  close() :void {
+    console.log('closing dialog');
+    this.dialogRef.close();
+  }
 }
