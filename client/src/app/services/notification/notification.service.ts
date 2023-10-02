f4c0efdba1f86b4f0d5d5b497252f16ee5786cc1
Hamza Khalid Chouhdary <hkchouhdary@gmail.com>
2023-10-02 14:30:58 +0000

    added new notification method

--- client/src/app/services/notification/notification.service.ts
+++ client/src/app/services/notification/notification.service.ts
@@ -11,4 +11,7 @@ export class NotificationService {
   showSuccess(message: string, title: string = 'Success') {
     this.toastr.success(message, title);
   }
+  showError(message: string, title: string = 'Error') {
+    this.toastr.error(message, title);
+  }
 }
